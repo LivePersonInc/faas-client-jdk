84481c9ec6ff79190a6f572a9261066bbe0fc1e9
eplaza <eplaza@liveperson.com>
2025-03-27 07:27:09 +0100

    Update JavaDocs

--- src/main/java/com/liveperson/faas/client/FaaSClient.java
+++ src/main/java/com/liveperson/faas/client/FaaSClient.java
@@ -12,106 +12,106 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * FaaS client for invoking lambdas of a account/brand
+ * FaaS client for invoking Functions of a account/brand
  *
  * @author sschwarz
  * @author arotaru
  */
 public interface FaaSClient {
         /**
-         * Invoking a lambda per brand via the RESTful api by event ID
+         * Invoking a function per brand via the RESTful api by event ID
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param responseType   the type of the response object
          * @param optionalParams optional Parameters for request - requestId and
          *                       timeOuts
          * @param <T>            Class of the response type
          * @return the response object of type responseType
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         <T> T invokeByEvent(String lpEventSource, FaaSEvent event, FaaSInvocation data, Class<T> responseType,
                         OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Invoking a lambda per brand via the RESTful api by event ID
+         * Invoking a function per brand via the RESTful api by event ID
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param responseType   the type of the response object
          * @param optionalParams optional Parameters for request - requestId and timeOut
          * @param <T>            Class of the response type
          * @return the response object of type responseType
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         <T> T invokeByEvent(String lpEventSource, String event, FaaSInvocation data, Class<T> responseType,
                         OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Invoking a lambda per brand via the RESTful api by event ID but does not
+         * Invoking a function per brand via the RESTful api by event ID but does not
          * return response of invocation
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param optionalParams optional Parameters for request - requestId and timeOut
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         void invokeByEvent(String lpEventSource, FaaSEvent event, FaaSInvocation data, OptionalParams optionalParams)
                         throws FaaSException;
 
         /**
-         * Invoking a lambda per brand via the RESTful api by event ID but does not
+         * Invoking a function per brand via the RESTful api by event ID but does not
          * return response of invocation
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param optionalParams optional Parameters for request - requestId and timeOut
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         void invokeByEvent(String lpEventSource, String event, FaaSInvocation data, OptionalParams optionalParams)
                         throws FaaSException;
 
         /**
-         * Invoking a lambda per brand via the RESTful api by lambda UUID
+         * Invoking a function per brand via the RESTful api by function UUID
          *
          * @param lpEventSource  the name of the source system doing the invocation
-         * @param functionUUID   the global unique identifier of a lambda
+         * @param functionUUID   the global unique identifier of a function
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param responseType   the type of the response object
          * @param optionalParams optional Parameters for request - requestId and
          *                       timeOuts
          * @param <T>            Class of the response type
          * @return the response object of type responseType
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         <T> T invokeByUUID(String lpEventSource, String functionUUID, FaaSInvocation data, Class<T> responseType,
                         OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Invoking a lambda per brand via the RESTful api by lambda UUID but does not
+         * Invoking a function per brand via the RESTful api by function UUID but does not
          * return response of invocation
          *
          * @param lpEventSource  the name of the source system doing the invocation
-         * @param functionUUID   the global unique identifier of a lambda
+         * @param functionUUID   the global unique identifier of a function
          * @param data           the invocation payload containing timestamp (ms),
-         *                       headers and payload send to the lambda
+         *                       headers and payload send to the function
          * @param optionalParams optional Parameters for request - requestId and timeOut
-         * @throws FaaSException when error occurs during lambda invocation
+         * @throws FaaSException when error occurs during function invocation
          */
         void invokeByUUID(String lpEventSource, String functionUUID, FaaSInvocation data, OptionalParams optionalParams)
                         throws FaaSException;
 
         /**
-         * Checking if lambda implementation exist for a given event
+         * Checking if function implementation exist for a given event
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
@@ -123,7 +123,7 @@ public interface FaaSClient {
                         throws FaaSException;
 
         /**
-         * Checking if lambda implementation exist for a given event
+         * Checking if function implementation exist for a given event
          *
          * @param lpEventSource  the name of the source system doing the invocation
          * @param event          the faas event
@@ -134,7 +134,7 @@ public interface FaaSClient {
         boolean isImplemented(String lpEventSource, String event, OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Get a list of lambdas. Filters can be applied by state, eventId or name.
+         * Get a list of V1 Functions. Filters can be applied by state, eventId or name.
          * EventId and name supports substring.
          *
          * @param userId              LE user id
@@ -143,20 +143,19 @@ public interface FaaSClient {
          *                            eventId, name
          * @param optionalParams      optional Parameters for request - requestId and
          *                            timeOut
-         * @return A list of lambdas that belong that the above account filtered by the
+         * @return A list of functions that belong that the above account filtered by the
          *         optionalQueryParams
-         * @throws FaaSException when error occurs during getLambda request
-         * @deprecated compatible with V1 and V2 Functions, using 'getFunctions' is
-         *             recomended for V2 functions,
+         * @throws FaaSException when error occurs during getLambdas request
+         * @deprecated Compatible Only with V1 Functions. For V2 use 'getFunctions' instead.
          * 
          */
         List<LambdaResponse> getLambdas(String userId, Map<String, String> optionalQueryParams,
                         OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Get a list of Functions. Only V2 Filters can be applied by state, eventId or
-         * name.
-         * EventId and name supports substring.
+         * Get a list of V2 Functions. Filters can be applied by state, eventId or
+         * functionName.
+         * EventId and functionName supports substring.
          *
          * @param userId              LE user id
          * @param optionalQueryParams optional query params that are used for filtering
@@ -164,20 +163,19 @@ public interface FaaSClient {
          *                            eventId, functionName
          * @param optionalParams      optional Parameters for request - requestId and
          *                            timeOut
-         * @return A list of Functions that belong that the above account filtered by
-         *         the
-         *         optionalQueryParams
-         * @throws FaaSException when error occurs during getLambda request
+         * @return A list of Functions that belong to the above account filtered by
+         *         the optionalQueryParams
+         * @throws FaaSException when error occurs during getFunctions request
          */
         List<FunctionResponse> getFunctions(String userId, Map<String, String> optionalQueryParams,
                         OptionalParams optionalParams) throws FaaSException;
 
         /**
-         * Determines if the accounts associated to the client instance has V2 domain
-         * from the CSDS. Only will consider the GW domain. IF GW domain is V2 Is
-         * assumed that the Functions UI domain is also V2
+         * Determines whether the accounts associated with the client instance have a V2
+         * domain from the CSDS. It only considers the GW domain. If the GW domain is
+         * V2, it is assumed that the UI domain is also V2.
          * 
-         * @return true is Client instance account is V2
+         * @return true if Client instance account is V2.
          * @throws CsdsRetrievalException
          */
         public boolean isV2Domain() throws CsdsRetrievalException;
